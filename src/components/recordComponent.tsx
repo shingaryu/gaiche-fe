4a6ad5a09a44fcb6289b84d04b2f645ac9f11900
Koki Matsuzawa <shingaryu@gmail.com>
2019-09-29 23:58:56 +0200

    Add draft visuals of RecordComponent

--- src/components/recordComponent.tsx
+++ src/components/recordComponent.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Button } from 'react-bootstrap';
+import { Button, ListGroup, Form, FormControl, Container, Row, Col } from 'react-bootstrap';
 import TimeSeriesBalance from "../models/TimeSeriesBalance";
 import RecordApi from "../api/recordApi";
+import './recordComponent.css';
 
 type RecordComponentProps = {
-  // recordApi: RecordApi
+
 }
 
 type RecordComponentState = {
@@ -12,28 +13,60 @@ type RecordComponentState = {
 }
 
 class RecordComponent extends React.Component<RecordComponentProps, RecordComponentState> {
+  state: RecordComponentState;
+  recordApi: RecordApi;
+
   constructor(props: RecordComponentProps) {
     super(props);
     this.state = { records: []};
-    const recordApi = new RecordApi();
-    recordApi.getTimeSeriesBalanceAll('YEN', 0).then((data) => {
+    this.recordApi = new RecordApi();
+    this.recordApi.getTimeSeriesBalanceAll('YEN', 0).then((data) => {
       this.setState( {records: data.data});
-    }).catch(e => console.log('recordApi.getTimeSeriesBalanceAll error ' + e));
+    }).catch(e => {
+      console.log('recordApi.getTimeSeriesBalanceAll error ' + e)
+    });
   }
 
   render() {
     return (
-      <div>
-        hello from RecordComponent!
-        <Button>Save</Button>
-        {this.state.records && this.state.records.map(record => {
-          return (
-            <div>
-              date: {record.date}, balance: {record.balance}
-            </div> 
-          )
-        })};
-      </div>
+      <Container fluid>
+        <h1>Records</h1>
+        <Form className="api-form">
+          <Form.Row>
+            <Col md={2}>
+              <Form.Label>Base currency</Form.Label>
+              <Form.Control inline as="select">
+              <option>YEN</option>
+              <option>EUR</option>
+              <option>USD</option>
+              </Form.Control>
+            </Col>
+            <Col md={2}>
+              <Form.Label>Initial amount</Form.Label>
+              <Form.Control type="number" placeholder="0" />
+            </Col>
+            <Col md={2} className="form-line">
+              <Button variant="outline-success">Update</Button>
+            </Col>
+
+          </Form.Row>
+        </Form>
+        <Row>
+          <Col>
+            <label>Time series balance</label>
+            <ListGroup>
+              {this.state.records && this.state.records.map(record => {
+                return (
+                  <ListGroup.Item>
+                    <span className="date">{record.date}</span>
+                    <span className="balance">{record.balance}</span>
+                  </ListGroup.Item> 
+                )
+              })}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }

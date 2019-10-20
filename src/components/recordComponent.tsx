f831e998cd0834793febab4c3561f1b377748b66
Koki Matsuzawa <shingaryu@gmail.com>
2019-10-21 00:01:22 +0200

    Add chart to RecordComponent

--- src/components/recordComponent.tsx
+++ src/components/recordComponent.tsx
@@ -3,6 +3,7 @@ import { Button, ListGroup, Form, FormControl, Container, Row, Col } from 'react
 import TimeSeriesBalance from "../models/TimeSeriesBalance";
 import RecordApi from "../api/recordApi";
 import './recordComponent.css';
+import {Line} from 'react-chartjs-2';
 
 type RecordComponentProps = {
 
@@ -15,6 +16,8 @@ type RecordComponentState = {
 class RecordComponent extends React.Component<RecordComponentProps, RecordComponentState> {
   state: RecordComponentState;
   recordApi: RecordApi;
+  chartData: any;
+  chartOptions: any;
 
   constructor(props: RecordComponentProps) {
     super(props);
@@ -28,6 +31,45 @@ class RecordComponent extends React.Component<RecordComponentProps, RecordCompon
   }
 
   render() {
+    this.chartData = {
+      // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'balance',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.state.records.map(record => { return { t: record.date, y: record.balance }})
+        }
+      ]
+    };
+  
+    this.chartOptions = {
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {
+            min: this.state.records.length > 0 && this.state.records[0].date,
+            unit: 'day'
+        }
+        }]
+      }
+    }  
+
     return (
       <Container fluid>
         <h1>Records</h1>
@@ -53,7 +95,11 @@ class RecordComponent extends React.Component<RecordComponentProps, RecordCompon
         </Form>
         <Row>
           <Col>
-            <label>Time series balance</label>
+            <div>
+            <label>Time series balance (chart)</label>
+              <Line data={this.chartData} options={this.chartOptions}/>
+            </div>
+            <label>Time series balance (table)</label>
             <ListGroup>
               {this.state.records && this.state.records.map(record => {
                 return (

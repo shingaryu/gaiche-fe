90f23ea505e0abd84f974f958ef02da5f63f9458
Koki Matsuzawa <shingaryu@gmail.com>
2019-11-17 16:22:23 +0100

    Add Auth0 authentication

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import RecordComponent from './components/recordComponent';
 import HeaderComponent from './components/headerComponent';
+import { useAuth0 } from "./services/react-auth0-spa";
+import NavBar from "./components/navBarComponent";
 
 function App() {
-  return (
-    <div>
-      <header>
-        <HeaderComponent/>
-      </header>
-      <RecordComponent/>
-    </div>
-  );
+  const { loading, isAuthenticated } = useAuth0();
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  } else if (!isAuthenticated) {
+    return (
+      <div>
+        <div>
+          Please Login!
+        </div>
+        <NavBar />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <header>
+          <NavBar />
+          <HeaderComponent/>
+        </header>
+        <RecordComponent/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Auth0Provider } from "./services/react-auth0-spa";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = (appState: any) => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    client_id={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />, 
+  </Auth0Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
